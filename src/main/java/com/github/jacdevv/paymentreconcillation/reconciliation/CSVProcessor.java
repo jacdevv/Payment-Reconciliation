1265409d8c3b08cc8de892c4585fb88d1c83f814
jacdevv <main.jac.mil@gmail.com>
2026-08-16 18:45:24 +0700

    feat(csv): supports function

--- src/main/java/com/github/jacdevv/paymentreconcillation/reconciliation/CSVProcessor.java
+++ src/main/java/com/github/jacdevv/paymentreconcillation/reconciliation/CSVProcessor.java
@@ -3,8 +3,6 @@ package com.github.jacdevv.paymentreconcillation.reconciliation;
 public class CSVProcessor implements FileProcessor {
     @Override
     public boolean supports(String filename) {
-
+        return filename != null &&  filename.trim().toLowerCase().endsWith(".csv");
     }
-
-
 }
